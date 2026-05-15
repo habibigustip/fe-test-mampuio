0e6ac2e1b5bc8fafa5db5063cd454643e89bc9f9
Habibi Gusti Pangestu <habibigustip@gmail.com>
2026-05-15 21:31:43 +0700

    feat: styling homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,17 @@
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans">
-      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white sm:items-start">
-      </main>
-    </div>
+    <main className="flex flex-1 w-full flex-col items-center justify-between py-32 px-16 bg-white sm:items-start">
+      <section className="flex flex-1 flex-col w-full items-center justify-center">
+        <div className="text-3xl">
+          Hi, I&apos;m <span className="font-semibold">Habibi Gusti Pangestu</span>
+        </div>
+        <div className="text-gray-500">
+          Thank you for giving me the opportunity
+        </div>
+        <div className="mt-6">
+          You can check the progress of the take home test in the <span className="font-semibold">users</span> menu in the top right corner of the navbar.
+        </div>
+      </section>
+    </main>
   );
 }
